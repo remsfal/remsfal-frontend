6fc32ca09ef200fd29c591dd429a4de529715d8d
Alexander Stanik <alexander.stanik@htw-berlin.de>
2024-06-04 11:01:39 +0200

    removed unused args

--- vite.config.ts
+++ vite.config.ts
@@ -22,15 +22,15 @@ export default defineConfig({
         secure: false,
         xfwd: true,
         ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('Proxy Error', err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+          proxy.on('proxyReq', (proxyReq, req) => {
+            console.log('Proxy Request to Backend:', req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          proxy.on('proxyRes', (proxyRes, req) => {
+            console.log('Proxy Response from Backend:', proxyRes.statusCode, req.url);
           });
         }
       }
