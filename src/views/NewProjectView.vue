993d97a6be1b7d4a8b9919d6df54531327717cd4
Hassan <Hassan.Alhawari@jaemacom.de>
2024-06-19 11:53:52 +0200

    Adjusted card positioning to center and resolved error message display issue

--- src/views/NewProjectView.vue
+++ src/views/NewProjectView.vue
@@ -1,49 +1,80 @@
 <script lang="ts">
-import ProjectService, {type Project} from "@/services/ProjectService";
+import ProjectService, { type Project } from "@/services/ProjectService";
 
 export default {
   data() {
     return {
       projectTitle: "",
-      errorMessage: "Der Projekttitel darf nicht mehr als 250 Zeichen lang sein",
+      errorMessage: "",
+      maxLength: 100,
     };
   },
   methods: {
     createProject() {
-
       const projectService = new ProjectService();
+
       // Now project_title will contain the value emitted from the pressedButton event
+
+      if (this.projectTitle.length > this.maxLength) return;
+
       projectService
         .createProject(this.projectTitle)
         .then((newProject: Project) => {
           console.info("new project has been created: ", newProject);
-          this.$router.push({name: "ProjectDashboard", params: {projectId: newProject.id}});
-        })
+          this.$router.push({
+            name: "ProjectDashboard",
+            params: { projectId: newProject.id },
+          });
+        });
     },
     abort() {
-      this.$router.push({name: "ProjectSelection"})
-    }
+      this.$router.push({ name: "ProjectSelection" });
+    },
+  },
+  watch: {
+    projectTitle(newVal) {
+      if (newVal.length > this.maxLength) {
+        this.errorMessage = `Der Projekttitel darf nicht mehr als ${this.maxLength} Zeichen lang sein`;
+      } else {
+        this.errorMessage = "";
+      }
+    },
   },
 };
 </script>
 
 <template>
-  <div class="grid">
-    <div class="card flex justify-content-center">
-      <!-- form @submit="onSubmit" class="flex flex-column gap-2"-->
-      <form @submit.prevent="createProject" class="flex flex-column gap-2">
-            <span class="p-float-label">
-                <InputText id="value" v-model="projectTitle" type="text" :class="{ 'p-invalid': errorMessage }" aria-describedby="text-error" />
-                <label for="value">Projekttitel</label>
-            </span>
-        <small class="p-error" id="text-error">{{ errorMessage || '&nbsp;' }}</small>
-        <Button type="submit" label="Erstellen" icon="pi pi-plus" iconPos="left"/>
+  <div class="grid align-items-center justify-content-center mt-5">
+    <div class="card flex">
+      <form
+        @submit.prevent="createProject"
+        class="flex flex-column gap-2 w-23rem"
+      >
+        <span class="p-float-label">
+          <InputText
+            id="value"
+            v-model="projectTitle"
+            type="text"
+            :class="{ 'p-invalid': errorMessage }"
+            aria-describedby="text-error"
+          />
+          <label for="value">Projekttitel</label>
+        </span>
+        <small class="p-error" id="text-error">{{
+          errorMessage || "&nbsp;"
+        }}</small>
         <Button
-            @click="abort"
-            type="reset"
-            label="Abbrechen"
-            icon="pi pi-times"
-            iconPos="left"
+          type="submit"
+          label="Erstellen"
+          icon="pi pi-plus"
+          iconPos="left"
+        />
+        <Button
+          @click="abort"
+          type="reset"
+          label="Abbrechen"
+          icon="pi pi-times"
+          iconPos="left"
         />
       </form>
     </div>
