fda5a5b9c581e7c80d5b5a8fda115d075e83b8a0
Tobiwue <s0573300@htw-berlin.de>
2024-06-21 15:31:01 +0200

    mock user and test project file

--- cypress/e2e/LandingPage.cy.ts
+++ cypress/e2e/LandingPage.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Test Landing Page', () => {
   it('User is logged out by visiting the app root url', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:5173/')
     cy.title().should('eq', 'remsfal Online - Cloud Service for Facility Management')
   })
 })
