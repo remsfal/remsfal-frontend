bea1c61df92a4704bb6f473718b48e9697782894
Tobiwue <s0573300@htw-berlin.de>
2024-06-21 15:40:54 +0200

    fix

--- cypress/e2e/LandingPage.cy.ts
+++ cypress/e2e/LandingPage.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Test Landing Page', () => {
   it('User is logged out by visiting the app root url', () => {
-    cy.visit('http://localhost:5173/')
+    cy.visit('/')
     cy.title().should('eq', 'remsfal Online - Cloud Service for Facility Management')
   })
 })
