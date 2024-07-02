2d33962d2c07b837546697fbb7a481050dbcf3f6
ruppfabian1997 <ruppfabian97@gmail.com>
2024-07-02 13:55:46 +0200

    edit task management feature

--- src/layout/ManagerMenu.vue
+++ src/layout/ManagerMenu.vue
@@ -1,47 +1,72 @@
-AppMenu.vue<script setup lang="ts">
+<script setup lang="ts">
 import { ref } from 'vue';
-
 import AppMenuItem from './AppMenuItem.vue';
+import { useProjectStore } from '@/stores/ProjectStore';
+import { useRouter } from 'vue-router';
+import {useUserSessionStore} from "@/stores/UserSession";
+import {Status} from "@/services/TaskService";
+
+const router = useRouter();
+const projectStore = useProjectStore();
+const sessionStore = useUserSessionStore();
+
 
 const model = ref([
-    {
-        label: 'Home',
-        items: [
-            { label: 'Dashboard', icon: 'pi pi-fw pi-chart-bar', to: '/' },
-            { label: 'Einstellungen', icon: 'pi pi-fw pi-cog', to: '/' }
-        ]
-    },
-    {
-        label: 'Stammdaten',
-        items: [
-            { label: 'Objektdaten', icon: 'pi pi-fw pi-home', to: '/uikit/formlayout' },
-            { label: 'Mieter', icon: 'pi pi-fw pi-users', to: '/uikit/input' }
-        ]
-    },
-    {
-        label: 'Aufgabenmanagement',
-        items: [
-          { label: 'Meine Aufgaben', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
-          { label: 'Offene Aufgaben', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
-          { label: 'Alle Aufgaben', icon: 'pi pi-fw pi-list', to: '/uikit/input' }
-        ]
-    },
-    {
-        label: 'Mängelmanagement',
-        items: [
-          { label: 'Neue Meldungen', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
-          { label: 'Offene Mängel', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
-          { label: 'Beauftragte Mängel', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
-          { label: 'Alle Meldungen', icon: 'pi pi-fw pi-list', to: '/uikit/input' }
-        ]
-    }
+  {
+    label: 'Home',
+    items: [
+      { label: 'Dashboard', icon: 'pi pi-fw pi-chart-bar', to: '/' },
+      { label: 'Einstellungen', icon: 'pi pi-fw pi-cog', to: '/' }
+    ]
+  },
+  {
+    label: 'Stammdaten',
+    items: [
+      { label: 'Objektdaten', icon: 'pi pi-fw pi-home', to: '/uikit/formlayout' },
+      { label: 'Mieter', icon: 'pi pi-fw pi-users', to: '/uikit/input' }
+    ]
+  },
+  {
+    label: 'Aufgabenmanagement',
+    items: [
+      { label: 'Meine Aufgaben',
+        icon: 'pi pi-fw pi-list',
+        navigate: () => {
+          const projectId = projectStore.selectedProject?.id;
+          router.push({ name: 'MyTasksOverview', params: { projectId }, query: { owner: sessionStore.user?.id } });
+        } },
+      { label: 'Offene Aufgaben',
+        icon: 'pi pi-fw pi-list',
+        navigate: () => {
+          const projectId = projectStore.selectedProject?.id;
+          router.push({ name: 'OpenTasksOverview', params: { projectId }, query: { status: Status.OPEN} });
+        }},
+      {
+        label: 'Alle Aufgaben',
+        icon: 'pi pi-fw pi-list',
+        navigate: () => {
+          const projectId = projectStore.selectedProject?.id;
+          router.push({ name: 'AllTasksOverview', params: { projectId } });
+        }
+      }
+    ]
+  },
+  {
+    label: 'Mängelmanagement',
+    items: [
+      { label: 'Neue Meldungen', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
+      { label: 'Offene Mängel', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
+      { label: 'Beauftragte Mängel', icon: 'pi pi-fw pi-list', to: '/uikit/formlayout' },
+      { label: 'Alle Meldungen', icon: 'pi pi-fw pi-list', to: '/uikit/input' }
+    ]
+  }
 ]);
 </script>
 
 <template>
-    <ul class="layout-menu">
-        <template v-for="(item, i) in model" :key="item">
-            <app-menu-item :item="item" :index="i"></app-menu-item>
-        </template>
-    </ul>
+  <ul class="layout-menu">
+    <template v-for="(item, i) in model" :key="item.label">
+      <app-menu-item :item="item" :index="i" />
+    </template>
+  </ul>
 </template>
