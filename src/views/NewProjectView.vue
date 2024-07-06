bde8772cc80984669ddf8f42c4d56657979ab974
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2024-07-06 12:14:45 +0200

    revert faulty commits

--- src/views/NewProjectView.vue
+++ src/views/NewProjectView.vue
@@ -1,82 +1,17 @@
 <script lang="ts">
-import ProjectService, { type Project } from "@/services/ProjectService";
+import NewProjectForm from "@/components/NewProjectForm.vue";
+import {defineComponent} from "vue";
 
-export default {
-  data() {
-    return {
-      projectTitle: "",
-      errorMessage: "",
-      maxLength: 100,
-    };
-  },
-  methods: {
-    createProject() {
-      const projectService = new ProjectService();
+export default defineComponent({
+  components: {NewProjectForm}
+})
 
-      // Now project_title will contain the value emitted from the pressedButton event
-
-      if (this.projectTitle.length > this.maxLength) return;
-
-      projectService
-        .createProject(this.projectTitle)
-        .then((newProject: Project) => {
-          console.info("new project has been created: ", newProject);
-          this.$router.push({
-            name: "ProjectDashboard",
-            params: { projectId: newProject.id },
-          });
-        });
-    },
-    abort() {
-      this.$router.push({ name: "ProjectSelection" });
-    },
-  },
-  watch: {
-    projectTitle(newVal) {
-      if (newVal.length > this.maxLength) {
-        this.errorMessage = `Der Projekttitel darf nicht mehr als ${this.maxLength} Zeichen lang sein`;
-      } else {
-        this.errorMessage = "";
-      }
-    },
-  },
-};
 </script>
 
 <template>
   <div class="grid align-items-center justify-content-center">
     <div class="card flex mt-5">
-      <form
-        @submit.prevent="createProject"
-        class="flex flex-column gap-2 w-23rem"
-      >
-        <span class="p-float-label">
-          <InputText
-            id="value"
-            v-model="projectTitle"
-            type="text"
-            :class="{ 'p-invalid': errorMessage }"
-            aria-describedby="text-error"
-          />
-          <label for="value">Projekttitel</label>
-        </span>
-        <small class="p-error" id="text-error">{{
-          errorMessage || "&nbsp;"
-        }}</small>
-        <Button
-          type="submit"
-          label="Erstellen"
-          icon="pi pi-plus"
-          iconPos="left"
-        />
-        <Button
-          @click="abort"
-          type="reset"
-          label="Abbrechen"
-          icon="pi pi-times"
-          iconPos="left"
-        />
-      </form>
+      <NewProjectForm></NewProjectForm>
     </div>
   </div>
 </template>
--- src/views/ProjectSelectionView.vue
+++ src/views/ProjectSelectionView.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
-import { defineComponent, computed, ref, watch } from 'vue';
+import { defineComponent, computed, ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useProjectStore } from '@/stores/ProjectStore';
-import ProjectService, { type Project } from "@/services/ProjectService";
+import NewProjectForm from "@/components/NewProjectForm.vue";
+import ProjectService from "@/services/ProjectService";
 
 export default defineComponent({
+  components: { NewProjectForm },
   setup() {
     const projectStore = useProjectStore();
     const router = useRouter();
@@ -24,55 +26,100 @@ export default defineComponent({
 
     const onRowClick = (event: any) => {
       projectStore.setSelectedProject(event.data);
-      router.push({ name: "ProjectDashboard", params: { projectId: projectStore.projectId } });
+      router.push({ name: "ProjectDashboard", params: { projectId: event.data.id } });
     };
 
     const display = ref(false);
-    const projectTitle = ref("");
-    const errorMessage = ref("");
-    const maxLength = 100;
 
     const open = () => {
       display.value = true;
-      errorMessage.value = "";
-      projectTitle.value = "";
     };
 
     const close = () => {
       display.value = false;
     };
 
-    const createProject = () => {
-      const projectService = new ProjectService();
+    const dt = ref();
+    const loading = ref(false);
+    const totalRecords = ref(0);
+    const projects = ref([]);
+    const selectAll = ref(false);
+    const first = ref(0);
+    const rows = ref(10);
+    const filters = ref({
+      'name': { value: '', matchMode: 'contains' },
+      'id': { value: '', matchMode: 'contains' },
+      'memberGerman': { value: '', matchMode: 'contains' }
+    });
+    const lazyParams = ref({
+      limit: rows.value,
+      offset: first.value,
+      sortField: null,
+      sortOrder: null,
+      filters: filters.value
+    });
 
-      if (projectTitle.value.length > maxLength) {
-        errorMessage.value = `Der Projekttitel darf nicht mehr als ${maxLength} Zeichen lang sein`;
-        return;
+    const loadLazyData = (event = {}) => {
+      loading.value = true;
+
+      if (event.first !== undefined) {
+        lazyParams.value.offset = event.first;
+      }
+      if (event.rows !== undefined) {
+        lazyParams.value.limit = event.rows;
+      }
+      if (event.sortField !== undefined) {
+        lazyParams.value.sortField = event.sortField;
+      }
+      if (event.sortOrder !== undefined) {
+        lazyParams.value.sortOrder = event.sortOrder;
+      }
+      if (event.filters !== undefined) {
+        lazyParams.value.filters = event.filters;
       }
 
-      projectService.createProject(projectTitle.value).then((newProject: Project) => {
-        console.info("new project has been created: ", newProject);
-        router.push({ name: "ProjectDashboard", params: { projectId: newProject.id } });
-      });
+      ProjectService.getProjects(lazyParams.value.limit, lazyParams.value.offset)
+          .then((data) => {
+            projects.value = data.projects;
+            totalRecords.value = data.total;
+            loading.value = false;
+          })
+          .catch((error) => {
+            console.error(error);
+            loading.value = false;
+          });
     };
 
-    watch(projectTitle, (newVal) => {
-      if (newVal.length > maxLength) {
-        errorMessage.value = `Der Projekttitel darf nicht mehr als ${maxLength} Zeichen lang sein`;
-      } else {
-        errorMessage.value = "";
-      }
+    const onPage = (event) => {
+      loadLazyData(event);
+    };
+
+    const onSort = (event) => {
+      loadLazyData(event);
+    };
+
+    const onFilter = (event) => {
+      lazyParams.value.filters = filters.value;
+      loadLazyData(event);
+    };
+
+    onMounted(() => {
+      loadLazyData();
     });
 
     return {
-      projectList,
+      projects,
       onRowClick,
       display,
       open,
       close,
-      projectTitle,
-      errorMessage,
-      createProject,
+      loading,
+      totalRecords,
+      first,
+      rows,
+      onPage,
+      onSort,
+      onFilter
     };
   },
 });
@@ -84,14 +131,23 @@ export default defineComponent({
       <div class="card">
         <h5>Projekt Übersicht</h5>
         <DataTable
-            :value="projectList"
+            :value="projects"
+            lazy
+            paginator
+            :first="first"
+            :rows="rows"
             sortMode="single"
             scrollable
             scrollHeight="500px"
             @row-click="onRowClick"
             :rowHover="true"
-            :paginator="true"
-            :rows="10"
+            @page="onPage"
+            @sort="onSort"
+            @filter="onFilter"
+            :totalRecords="totalRecords"
+            :loading="loading"
+            filterDisplay="row"
+            :globalFilterFields="['name', 'id', 'memberGerman']"
         >
           <Column field="name" header="Titel" sortable style="min-width: 200px"></Column>
           <Column field="id" header="Projekt-ID" sortable style="min-width: 200px"></Column>
@@ -108,21 +164,9 @@ export default defineComponent({
           :style="{ width: '30vw' }"
           :modal="true"
       >
-        <form @submit.prevent="createProject" class="flex flex-column gap-2 w-23rem">
-          <span class="p-float-label">
-            <InputText
-                id="value"
-                v-model="projectTitle"
-                type="text"
-                :class="{ 'p-invalid': errorMessage }"
-                aria-describedby="text-error"
-            />
-            <label for="value">Projekttitel</label>
-          </span>
-          <small class="p-error" id="text-error">{{ errorMessage || "&nbsp;" }}</small>
-          <Button type="submit" label="Erstellen" icon="pi pi-plus" iconPos="left" />
-          <Button @click="close" type="reset" label="Abbrechen" icon="pi pi-times" iconPos="left" />
-        </form>
+        <div style="margin-top: 1.5em">
+          <NewProjectForm />
+        </div>
       </Dialog>
     </div>
   </div>
