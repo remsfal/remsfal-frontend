953c020ffba7e1855c23cbec3afbafbdff6abea6
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2024-07-06 13:41:08 +0200

    emit event for pressing "Abbrechen"

--- src/components/NewProjectForm.vue
+++ src/components/NewProjectForm.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
-import ProjectService, { type Project } from "@/services/ProjectService";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   data() {
     return {
       projectTitle: "",
@@ -11,24 +11,10 @@ export default {
   },
   methods: {
     createProject() {
-      const projectService = new ProjectService();
-
-      // Now project_title will contain the value emitted from the pressedButton event
-
-      if (this.projectTitle.length > this.maxLength) return;
-
-      projectService
-          .createProject(this.projectTitle)
-          .then((newProject: Project) => {
-            console.info("new project has been created: ", newProject);
-            this.$router.push({
-              name: "ProjectDashboard",
-              params: { projectId: newProject.id },
-            });
-          });
+      this.$emit('createProject', this.projectTitle);
     },
-    abort() {
-      this.$router.push({ name: "ProjectSelection" });
+    emitAbort() {
+      this.$emit('abort');
     },
   },
   watch: {
@@ -40,39 +26,39 @@ export default {
       }
     },
   },
-};
+});
 </script>
 
 <template>
-      <form
-          @submit.prevent="createProject"
-          class="flex flex-column gap-2 w-23rem"
-      >
-        <span class="p-float-label">
-          <InputText
-              id="value"
-              v-model="projectTitle"
-              type="text"
-              :class="{ 'p-invalid': errorMessage }"
-              aria-describedby="text-error"
-          />
-          <label for="value">Projekttitel</label>
-        </span>
-        <small class="p-error" id="text-error">{{
-            errorMessage || "&nbsp;"
-          }}</small>
-        <Button
-            type="submit"
-            label="Erstellen"
-            icon="pi pi-plus"
-            iconPos="left"
-        />
-        <Button
-            @click="abort"
-            type="reset"
-            label="Abbrechen"
-            icon="pi pi-times"
-            iconPos="left"
-        />
-      </form>
+  <form @submit.prevent="createProject" class="flex flex-column gap-2 w-23rem">
+    <span class="p-float-label">
+      <InputText
+          id="value"
+          v-model="projectTitle"
+          type="text"
+          :class="{ 'p-invalid': errorMessage }"
+          aria-describedby="text-error"
+      />
+      <label for="value">Projekttitel</label>
+    </span>
+    <small class="p-error" id="text-error">
+      {{ errorMessage || "&nbsp;" }}
+    </small>
+    <Button
+        type="submit"
+        label="Erstellen"
+        icon="pi pi-plus"
+        iconPos="left"
+    />
+    <Button
+        @click="emitAbort"
+        type="reset"
+        label="Abbrechen"
+        icon="pi pi-times"
+        iconPos="left"
+    />
+  </form>
 </template>
+
+
+
