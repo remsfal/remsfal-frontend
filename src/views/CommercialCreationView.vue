a3de56cad5c5a59bb2bdcb8fdc70f5792f7e60db
Théo Thoumine <theo@thoumine.de>
2024-12-07 19:02:33 +0100

    Add creation form

---
+++ src/views/CommercialCreationView.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import ReusableForm from '../components/ReusableFormComponent.vue';
+import ProjectService, {type PropertyItem} from '@/services/ProjectService';
+
+const props = defineProps<{
+  projectId: string;
+  propertyId: string;
+  buildingId: string;
+}>();
+
+const fields = [
+  { name: 'name', label: 'Titel', type: 'text', required: true },
+  { name: 'location', label: 'Standort', type: 'textarea', required: false },
+  {
+    name: 'commercialSpace',
+    label: 'Gewerbefläche (qm)',
+    type: 'text',
+    validations: [
+      (value) => (!isNaN(value) ? null : 'Muss eine Zahl sein'),
+      // or null
+      (value) => (value > 0 ? null : 'Muss größer als 0 sein'),
+    ],
+  },
+  {
+    name: 'usableSpace',
+    label: 'Nutzfläche (qm)',
+    type: 'text',
+    validations: [
+      (value) => (!isNaN(value) ? null : 'Muss eine Zahl sein'),
+      (value) => (value > 0 ? null : 'Muss größer als 0 sein'),
+    ],
+  },
+  {
+    name: 'heatingSpace',
+    label: 'Heizfläche (qm)',
+    type: 'text',
+    validations: [
+      (value) => (!isNaN(value) ? null: 'Muss eine Zahl sein'),
+      (value) => (value > 0 ? null: 'Muss größer als 0 sein'),
+    ],
+  },
+  {
+    name: 'description',
+    label: 'Beschreibung',
+    type: 'textarea',
+    validations: [
+      (value) => (value.length <= 500 ? null : 'Beschreibung muss 500 Zeichen oder weniger sein'),
+    ],
+  },
+];
+
+const initialCommercialData = {
+  title: '',
+  location: '',
+  commercialSpace: '',
+  description: '',
+  propertyType: null,
+};
+
+const handleCommercialSubmit = (values: Record<string, any>) => {
+  const projectService = new ProjectService();
+  console.log('Submitting commercial:', values);
+  try {
+    const response = projectService.createCommercial(props.projectId, props.propertyId, props.buildingId, values);
+    console.log('Commercial created:', response);
+    // TODO go back to the overview
+  } catch (error) {
+    console.error('Error creating commercial:', error);
+
+  }
+};
+
+const handleCommercialCancel = () => {
+  console.log('Commercial creation canceled');
+  // TODO go back to the overview
+};
+</script>
+
+<template>
+  <ReusableForm
+      headline="Neue Gewerbeeinheit erstellen"
+      :fields="fields"
+      :initialValues="initialCommercialData"
+      saveButtonText="Gewerbeeinheit erstellen"
+      cancelButtonText="Abbrechen"
+      @submit="handleCommercialSubmit"
+      @cancel="handleCommercialCancel"
+  />
+</template>
