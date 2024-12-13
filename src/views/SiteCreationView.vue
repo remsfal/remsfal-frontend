a85c5a9f9d159ddd02c73f6bac539c98e51854cb
ZieglerAaron <s0588522@htw-berlin.de>
2024-12-13 13:57:09 +0100

    Extract SiteFormComponent.vue

--- src/views/SiteCreationView.vue
+++ src/views/SiteCreationView.vue
@@ -9,81 +9,11 @@ const props = defineProps<{
   propertyId: string;
 }>();
 
-// Define the form fields based on the SiteJson structure
-const fields: {
-  name: string;
-  label: string;
-  type: "select" | "textarea" | "text" | "checkbox";
-  options?: any[];
-  required?: boolean;
-  validations?: ((value: any) => string | null)[];
-}[] = [
-  {
-    name: 'title',
-    label: 'Title',
-    type: 'text',
-    required: true,
-    validations: [
-      (value: string) => (value.length > 255 ? 'Ein Titel darf nicht mehr als 255 Zeichen lang sein.' : null),
-    ],
-  },
-  {
-    name: 'description',
-    label: 'Description',
-    type: 'textarea',
-  },
-  {
-    name: 'usableSpace',
-    label: 'Usable Space (sqm)',
-    type: 'text',
-    validations: [
-      (value) => (!isNaN(value) ? null : 'Muss eine Zahl sein'),
-    ],
-  },
-  {
-    name: 'street',
-    label: 'Straße und Hausnummer',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'city',
-    label: 'Stadt',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'zip',
-    label: 'PLZ',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'province',
-    label: 'Bundesland',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'country',
-    label: 'Land',
-    type: 'text',
-    required: false,
-  },
-];
-
-const initialValues = {
-  title: '',
-  description: '',
-  usableSpace: '',
-  address: '',
-};
-
 const loading = ref(false);
 const error = ref<string | null>(null);
 
 // Submit handler
-const handleSubmit = async (formValues: any) => {
+const handleSubmit = async (formValues: Record<string, any>) => {
   loading.value = true;
   error.value = null;
   const siteService = new SiteService();
@@ -108,10 +38,9 @@ const handleSubmit = async (formValues: any) => {
   try {
     // Create the site
     console.log('Creating site:', site);
-    await projectService.createSite(props.projectId, props.propertyId, site);
-    alert('Eine Außenanlage wurde erfolgreich erstellt!');
-    //window.history.back();
     const resp = siteService.createSite(props.projectId, props.propertyId, site);
+    console.log('Site created:', resp);
+    // Here we could handle how to go back to the previous page
   } catch (err) {
     error.value = 'Außenanlage konnte nicht erstellt werden.' + err;
   } finally {
@@ -122,22 +51,22 @@ const handleSubmit = async (formValues: any) => {
 // Cancel handler
 const handleCancel = () => {
   alert('Form submission cancelled');
-  //window.history.back();
+  // Here we could handle how to go back to the previous page
 };
 </script>
 
 <template>
   <div>
-    <ReusableFormComponent
+    <SiteFormComponent
+        :projectId="projectId"
+        :propertyId="propertyId"
         headline="Neue Außenanlage erstellen"
         saveButtonText="Außenanlage erstellen"
         cancelButtonText="Abbrechen"
-        :fields="fields"
-        :initialValues="initialValues"
-        @submit="handleSubmit"
-        @cancel="handleCancel"
+        :onSubmit="handleSubmit"
+        :onCancel="handleCancel"
     />
-    <div v-if="loading">Submitting...</div>
+    <div v-if="loading">Senden...</div>
     <div v-if="error" class="error-message">{{ error }}</div>
   </div>
 </template>
--- src/views/SiteUpdateView.vue
+++ src/views/SiteUpdateView.vue
@@ -6,100 +6,32 @@ import type {AddressItem} from "@/services/ProjectService";
 
 const props = defineProps<{
   projectId: string;
-  propertyId: string;
+  propertyId?: string;
   siteId: string;
 }>();
 
-// Define the form fields based on the SiteJson structure
-const fields: {
-  name: string;
-  label: string;
-  type: "select" | "textarea" | "text" | "checkbox";
-  options?: any[];
-  required?: boolean;
-  validations?: ((value: any) => string | null)[];
-}[] = [
-  {
-    name: 'title',
-    label: 'Title',
-    type: 'text',
-    required: true,
-    validations: [
-      (value: string) => (value.length > 255 ? 'Ein Titel darf nicht mehr als 255 Zeichen lang sein.' : null),
-    ],
-  },
-  {
-    name: 'description',
-    label: 'Description',
-    type: 'textarea',
-  },
-  {
-    name: 'usableSpace',
-    label: 'Usable Space (sqm)',
-    type: 'text',
-    validations: [
-      (value) => (!isNaN(value) ? null : 'Muss eine Zahl sein'),
-    ],
-  },
-  {
-    name: 'street',
-    label: 'Straße und Hausnummer',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'city',
-    label: 'Stadt',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'zip',
-    label: 'PLZ',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'province',
-    label: 'Bundesland',
-    type: 'text',
-    required: false,
-  },
-  {
-    name: 'country',
-    label: 'Land',
-    type: 'text',
-    required: false,
-  },
-];
+const initialValues: Record<string, any> = ref({})
 
-const initialSiteData = ref({
-  title: '',
-  description: '',
-  usableSpace: '',
-  street: '',
-  city: '',
-  zip: '',
-  province: '',
-  country: '',
-});
-
-onMounted( async ()=> {
-  const projectService = new ProjectService();
+let parentPropertyId: string | undefined = props.propertyId ? props.propertyId.valueOf() : undefined;
+onMounted(async () => {
+  if(!props.siteId) return;
+  const siteService = new SiteService();
   try {
     console.log('Fetching site data...');
-    const site = await projectService.getSite(props.projectId, props.propertyId, props.siteId);
-    initialSiteData.value = {
-      title: site.title,
-      description: site.description,
-      usableSpace: site.usableSpace.toString(),
-      street: site.address.street,
-      city: site.address.city,
-      zip: site.address.zip,
-      province: site.address.province,
-      country: site.address.country,
-    };
-    console.log('Site data:', initialSiteData.value);
+    const site = await siteService.getSite(props.projectId, props.siteId);
+    initialValues.value.title = site.title;
+    initialValues.value.description = site.description;
+    initialValues.value.usableSpace = site.usableSpace.toString();
+    initialValues.value.street = site.address.street;
+    initialValues.value.city = site.address.city;
+    initialValues.value.zip = site.address.zip;
+    initialValues.value.province = site.address.province;
+    initialValues.value.country = site.address.country;
+
+    if(!parentPropertyId) {
+      parentPropertyId = site.propertyId;
+    }
+    console.log('Site data:', initialValues.value);
   } catch (error) {
     console.error('Error fetching site data:', error);
   }
@@ -123,21 +55,18 @@ const handleSubmit = async (formValues: any) => {
   };
 
   const site: SiteItem = {
-    propertyId: props.propertyId,
+    propertyId: parentPropertyId ? parentPropertyId.valueOf() : '',
     title: formValues.title,
     description: formValues.description,
     usableSpace: parseFloat(formValues.usableSpace),
     address: address,
   };
 
-
   try {
     // Create the site
     console.log('Creating site:', site);
-    await projectService.updateSite(props.projectId, props.propertyId, props.siteId, site);
-    alert('Eine Außenanlage wurde erfolgreich aktualisiert!');
-    //window.history.back();
     await siteService.updateSite(props.projectId, props.siteId, site, props.propertyId);
+  // Here we could handle how to go back to the previous page
   } catch (err) {
     error.value = 'Außenanlage konnte nicht aktualisiert werden.' + err;
   } finally {
@@ -148,22 +77,24 @@ const handleSubmit = async (formValues: any) => {
 // Cancel handler
 const handleCancel = () => {
   alert('Form submission cancelled');
-  //window.history.back();
+  // Here we could handle how to go back to the previous page
 };
 </script>
 
 <template>
   <div>
-    <ReusableFormComponent
-        headline="Neue Außenanlage erstellen"
-        saveButtonText="Außenanlage erstellen"
+    <SiteFormComponent
+        :projectId="projectId"
+        :propertyId="propertyId"
+        :siteId="siteId"
+        headline="Außenanlage bearbeiten"
+        saveButtonText="Speichern"
         cancelButtonText="Abbrechen"
-        :fields="fields"
-        :initialValues="initialSiteData"
-        @submit="handleSubmit"
-        @cancel="handleCancel"
+        :onSubmit="handleSubmit"
+        :onCancel="handleCancel"
+        :initialValues="initialValues"
     />
-    <div v-if="loading">Submitting...</div>
+    <div v-if="loading">Senden...</div>
     <div v-if="error" class="error-message">{{ error }}</div>
   </div>
 </template>
