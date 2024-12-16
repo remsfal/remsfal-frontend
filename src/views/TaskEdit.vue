6a4c99fba86d9dce5905d634f0448c20c68bdb86
nasrwihtw <Nasr.al-Sayaghi@Student.HTW-Berlin.de>
2024-12-16 16:30:34 +0100

    fix: reorder script and template blocks in TaskEdit.vue

--- src/views/TaskEdit.vue
+++ src/views/TaskEdit.vue
@@ -1,13 +1,13 @@
-<template>
-  <div>
-    <h1>Edit Task</h1>
-    <p>Task ID: {{ taskId }}</p>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const taskId = route.params.taskid as string;
 </script>
+
+<template>
+  <div>
+    <h1>Edit Task</h1>
+    <p>Task ID: {{ taskId }}</p>
+  </div>
+</template>
