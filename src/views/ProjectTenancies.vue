45d475bcf52a99eca424610f8880b82fa3a03a88
RybochkinaMarina <r.ybochkina@mail.ru>
2024-12-16 13:28:25 +0100

    deleted formatDate function

--- src/views/ProjectTenancies.vue
+++ src/views/ProjectTenancies.vue
@@ -58,12 +58,6 @@ function generateMockTenantData(): TenantItem[] {
   ];
 }
 
-function formatDate(date: Date): string {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-}
 
 function openAddDialog() {
   isEditMode.value = false;
