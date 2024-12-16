b80a142e2a4881b39638d5ffe05eaecf25f76eee
nasrwihtw <Nasr.al-Sayaghi@Student.HTW-Berlin.de>
2024-12-16 15:55:27 +0100

    Refactor TaskEdit.vue to use Composition API for retrieving taskId

--- src/views/TaskEdit.vue
+++ src/views/TaskEdit.vue
@@ -1,17 +1,13 @@
 <template>
-    <div>
-      <h1>Edit Task</h1>
-      <p>Task ID: {{ taskId }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    computed: {
-      taskId() {
-        return this.$route.params.taskid;
-      },
-    },
-  };
-  </script>
-  
+  <div>
+    <h1>Edit Task</h1>
+    <p>Task ID: {{ taskId }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const taskId = route.params.taskid as string; // Ensure correct typing
+</script>
