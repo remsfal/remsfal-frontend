eca234f23635d454c18c7c9cdf2efc6ef1f87bee
JavascriptFreak <aishwary081997@gmail.com>
2025-06-07 22:07:38 +0200

    Added new changes to make the project reactive by chnaging the interface for the menu item

--- src/layout/AppMenuItem.vue
+++ src/layout/AppMenuItem.vue
@@ -4,9 +4,8 @@ import { RouterLink, useRoute } from 'vue-router';
 import { useLayout } from '@/layout/composables/layout';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { useI18n } from 'vue-i18n';
-
 const props = withDefaults(defineProps<MenuItemProps>(), {
-  item: () => ({}) as MenuItem,
+  item: {} as MenuItem,
   index: 0,
   root: true,
   parentItemKey: undefined,
@@ -16,16 +15,19 @@ const { t } = useI18n();
 
 export interface MenuItem {
   label: string;
-  icon: { type: 'pi' | 'fa'; name: string | [string, string] } | null;
-  to: string | undefined;
-  url: string | undefined;
-  navigate: () => void;
-  command: (event: object) => void;
-  disabled: boolean;
-  visible: boolean;
-  class: string;
-  target: string;
-  items: MenuItem[];
+  icon:
+    | { type: 'pi'; name: string }              // PrimeIcons class name
+    | { type: 'fa'; name: string | [string, string] }  // FontAwesome icon (string or tuple)
+    | null;
+  to?: string;
+  url?: string;
+  navigate?: () => void;
+  command?: (event: object) => void;
+  disabled?: boolean;
+  visible?: boolean;
+  class?: string;
+  target?: string;
+  items?: MenuItem[];
 }
 
 interface MenuItemProps {
@@ -38,18 +40,18 @@ interface MenuItemProps {
 const route = useRoute();
 const { layoutState, setActiveMenuItem, toggleMenu } = useLayout();
 
-const isActiveMenu = ref<boolean>(false);
+const isActiveMenu = ref(false);
 const itemKey = ref<string | undefined>(undefined);
 
 onBeforeMount(() => {
   itemKey.value = props.parentItemKey
-    ? props.parentItemKey + '-' + props.index
+    ? `${props.parentItemKey}-${props.index}`
     : String(props.index);
 
-  const activeItem = layoutState.activeMenuItem;
+  const activeItem = layoutState.activeMenuItem || '';
 
   isActiveMenu.value =
-    activeItem === itemKey.value || activeItem ? activeItem.startsWith(itemKey.value + '-') : false;
+    activeItem === itemKey.value || activeItem.startsWith(itemKey.value + '-');
 });
 
 watch(
@@ -66,25 +68,17 @@ const itemClick = (event: Event, item: MenuItem) => {
     return;
   }
 
-  if (
-    (item.to || item.url) &&
-    (layoutState.staticMenuMobileActive || layoutState.overlayMenuActive)
-  ) {
+  if ((item.to || item.url) && (layoutState.staticMenuMobileActive || layoutState.overlayMenuActive)) {
     toggleMenu();
   }
 
-  if (item.command) {
-    item.command({ originalEvent: event, item: item });
-  }
+  item.command?.({ originalEvent: event, item });
+  item.navigate?.();
 
-  if (item.navigate) {
-    item.navigate();
-  }
-
-  const foundItemKey = item.items
+  const foundItemKey = item.items?.length
     ? isActiveMenu.value
       ? props.parentItemKey
-      : itemKey
+      : itemKey.value
     : itemKey.value;
 
   setActiveMenuItem(foundItemKey);
@@ -108,9 +102,9 @@ const checkActiveRoute = (item: MenuItem) => {
       @click="itemClick($event, item)"
     >
       <template v-if="item.icon">
-        <i v-if="item.icon.type === 'pi'" :class="item.icon.name" class="layout-menuitem-icon"></i>
+        <i v-if="item.icon?.type === 'pi'" :class="item.icon.name" class="layout-menuitem-icon"></i>
         <FontAwesomeIcon
-          v-else-if="item.icon.type === 'fa'"
+          v-else-if="item.icon?.type === 'fa'"
           :icon="item.icon.name"
           class="layout-menuitem-icon"
         />
@@ -126,9 +120,9 @@ const checkActiveRoute = (item: MenuItem) => {
       @click="itemClick($event, item)"
     >
       <template v-if="item.icon">
-        <i v-if="item.icon.type === 'pi'" :class="item.icon.name" class="layout-menuitem-icon"></i>
+        <i v-if="item.icon?.type === 'pi'" :class="item.icon.name" class="layout-menuitem-icon"></i>
         <FontAwesomeIcon
-          v-else-if="item.icon.type === 'fa'"
+          v-else-if="item.icon?.type === 'fa'"
           :icon="item.icon.name"
           class="layout-menuitem-icon"
         />
@@ -139,13 +133,13 @@ const checkActiveRoute = (item: MenuItem) => {
     <Transition v-if="item.items && item.visible !== false" name="layout-submenu">
       <ul v-show="root ? true : isActiveMenu" class="layout-submenu">
         <app-menu-item
-          v-for="(child, i) in item.items"
+          v-for="(child, i) in item.items || []"
           :key="child.label"
           :index="i"
           :item="child"
-          :parentItemKey="itemKey"
+          :parentItemKey="itemKey.value"
           :root="false"
-        ></app-menu-item>
+        />
       </ul>
     </Transition>
   </li>
