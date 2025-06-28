dc16f9100917c7a3e40c1e2ec86ddbc733d347c4
Bilal YIldiz <Bilal.Yildiz@Student.HTW-Berlin.de>
2025-06-28 23:14:59 +0200

    Kleiner fehler

--- test/views/ChatView.spec.ts
+++ test/views/ChatView.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { mount, flushPromises } from '@vue/test-utils'
 import ChatView from '../../src/views/ChatView.vue'
 import Button from 'primevue/button'
