7896f7d1d264f3afcfd27a154e2a17158edd274d
JavascriptFreak <aishwary081997@gmail.com>
2025-07-07 13:56:14 +0200

    added changes for the mocha to vitest update

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,6 +18,7 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/*', 'test/service-worker/**'],
       setupFiles: [resolve(__dirname, 'test/setup/vitest.setup.ts')],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      // include: ['src/**/*.test.ts', 'test/**/*.spec.ts'], // optional
     },
   }),
 );
