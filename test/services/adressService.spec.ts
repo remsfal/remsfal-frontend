c6cc0702145a8864a2b0b036ae2d8cb425776ac1
raphaelBleyer <raphael.bleyer@gmail.com>
2025-12-10 21:32:57 +0100

    changed tests, adressService related tests should pass

--- test/services/adressService.spec.ts
+++ test/services/adressService.spec.ts
@@ -1,30 +1,22 @@
 import { describe, test, expect } from 'vitest';
 import AddressService from '../../src/services/adressService';
 
-describe('AddressService', () => {
+describe('AddressService with MSW', () => {
   const addressService = new AddressService();
 
   test('getCityFromZip returns address data for valid zip', async () => {
-    const address = await addressService.getCityFromZip('10115');
-    expect(address).toBeDefined();
-    expect(address.city).toBeDefined();
-    expect(address.province).toBeDefined();
-    expect(address.countryCode).toBeDefined();
+    const address = await addressService.getCityFromZip('12345');
+    expect(address).toEqual({
+      city: 'Sample City',
+      countryCode: '',
+      province: '',
+      street: '',
+      zip: '12345',
+    });
   });
 
   test('getCityFromZip returns empty object when no addresses found', async () => {
     const address = await addressService.getCityFromZip('00000');
     expect(address).toEqual({});
-  });
-
-  test('getCityFromZip returns address with correct structure', async () => {
-    const address = await addressService.getCityFromZip('10115');
-    if (Object.keys(address).length > 0) {
-      expect(address).toHaveProperty('street');
-      expect(address).toHaveProperty('city');
-      expect(address).toHaveProperty('zip');
-      expect(address).toHaveProperty('province');
-      expect(address).toHaveProperty('countryCode');
-    }
   });
 });
