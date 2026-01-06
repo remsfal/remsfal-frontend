8815c4c76384a9ec969a1e8e1ba164fcf4691511
JavascriptFreak <aishwary081997@gmail.com>
2026-01-06 18:55:38 +0100

    Added changes for the changing prop name

--- src/views/IssueDescription.vue
+++ src/views/IssueDescription.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
-  import { ref, watch } from 'vue';
-  import Textarea from 'primevue/textarea'; 
-  
-  interface Props {
-    description: string;
-  }
-  
-  const props = defineProps<Props>();
-  const emit = defineEmits<{
-    (e: 'update:description', value: string): void
-  }>();
-  
-  const localDescription = ref(props.description);
-  
-  watch(() => props.description, (newValue) => {
+import { ref, watch } from "vue";
+import Textarea from "primevue/textarea";
+
+interface Props {
+  description: string;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<{
+  (e: "update:description", value: string): void;
+}>();
+
+const localDescription = ref(props.description);
+
+watch(
+  () => props.description,
+  (newValue) => {
     localDescription.value = newValue;
-  });
-  
-  watch(localDescription, (newValue) => {
-    emit('update:description', newValue);
-  });
-  </script>
-  
-  <template>
-    <div class="w-full">
-      <Textarea
-        v-model="localDescription"
-        auto-resize
-        rows="8"
-        class="w-full"
-        placeholder="Write markdown description here..."
-      />
-    </div>
-  </template>
-  
+  }
+);
+
+watch(localDescription, (newValue) => {
+  emit("update:description", newValue);
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <Textarea
+      v-model="localDescription"
+      :autoResize="true"
+      rows="8"
+      class="w-full"
+      placeholder="Write markdown description here..."
+    />
+  </div>
+</template>
