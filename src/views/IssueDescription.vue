3e9582da8d3b8b4b9e986cc26e9bf0b591e20664
JavascriptFreak <aishwary081997@gmail.com>
2026-01-06 19:15:14 +0100

    Added changes for the updating the code issue description

--- src/views/IssueDescription.vue
+++ src/views/IssueDescription.vue
@@ -1,38 +1,40 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
-import Textarea from "primevue/textarea";
-
-interface Props {
-  description: string;
-}
-
-const props = defineProps<Props>();
-const emit = defineEmits<{
-  (e: "update:description", value: string): void;
-}>();
-
-const localDescription = ref(props.description);
-
-watch(
-  () => props.description,
-  (newValue) => {
-    localDescription.value = newValue;
-  }
-);
-
-watch(localDescription, (newValue) => {
-  emit("update:description", newValue);
-});
-</script>
-
-<template>
-  <div class="w-full">
-    <Textarea
-      v-model="localDescription"
-      :autoResize="true"
-      rows="8"
-      class="w-full"
-      placeholder="Write markdown description here..."
-    />
-  </div>
-</template>
+  import { ref, watch } from "vue";
+  import Textarea from "primevue/textarea";
+  
+  const props = defineProps<{
+    description: string;
+  }>();
+  
+  const emit = defineEmits<{
+    (e: "update:description", value: string): void;
+  }>();
+  
+  const localDescription = ref(props.description);
+  
+  // Watch for prop changes
+  watch(
+    () => props.description,
+    (newValue) => {
+      localDescription.value = newValue;
+    }
+  );
+  
+  // Emit changes when localDescription changes
+  watch(localDescription, (newValue) => {
+    emit("update:description", newValue);
+  });
+  </script>
+  
+  <template>
+    <div class="w-full">
+      <Textarea
+        v-model="localDescription"
+        :autoResize="true"
+        rows="8"
+        class="w-full"
+        placeholder="Write markdown description here..."
+      />
+    </div>
+  </template>
+  
