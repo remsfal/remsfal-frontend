94f1db0acf7d3767bf0be2b0426937ba05a4cb67
Philipp Bogatchev <philipp.bogatchev@remsfal.online>
2026-06-11 11:09:17 +0200

    modified_at anzeigen

--- src/features/tenant/tenantIssues/components/TenantIssueCard.vue
+++ src/features/tenant/tenantIssues/components/TenantIssueCard.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
 
 const emit = defineEmits<{ select: [] }>();
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
 const statusSeverity = computed(() => {
   switch (props.issue.status) {
@@ -40,6 +40,19 @@ const typeLabel = computed(() => {
 });
 
 const issueNodeId = computed(() => props.issue.id?.split('-').pop() || props.issue.id);
+const modifiedAtLabel = computed(() => {
+  const modifiedAt = props.issue.modifiedAt;
+  if (!modifiedAt) {
+    return null;
+  }
+
+  const date = new Date(modifiedAt);
+  if (Number.isNaN(date.getTime())) {
+    return `${t('tenantIssues.card.updated')} ${modifiedAt}`;
+  }
+
+  return `${t('tenantIssues.card.updated')} ${date.toLocaleDateString(locale.value)}`;
+});
 
 const typeSeverity = computed(() => {
   switch (props.issue.type) {
@@ -85,6 +98,7 @@ const titleClass = computed(() => {
           </h3>
           <div class="mt-3 flex flex-wrap gap-2">
             <Tag :value="issueNodeId" severity="info" class="inline-flex w-fit" />
+            <Tag v-if="modifiedAtLabel" :value="modifiedAtLabel" severity="info" class="inline-flex w-fit" />
             <Tag :value="typeLabel" :severity="typeSeverity" class="inline-flex w-fit" />
             <Tag :value="statusLabel" :severity="statusSeverity" class="inline-flex w-fit" />
           </div>
