e63d023591b8b11478873b41fbb122c6fa891cef
Philipp Bogatchev <philipp.bogatchev@remsfal.online>
2026-07-06 09:50:25 +0200

    zurück

--- src/features/project/rentalAgreements/components/RentalDetailsForm.vue
+++ src/features/project/rentalAgreements/components/RentalDetailsForm.vue
@@ -99,8 +99,8 @@ function onSubmit(event: FormSubmitEvent) {
     operatingCostsPrepayment: formState.operatingCostsPrepayment?.value || undefined,
     heatingCostsPrepayment: formState.heatingCostsPrepayment?.value || undefined,
     billingCycle: formState.billingCycle?.value,
-    firstPaymentDate: toISODateString(firstPaymentDateValue.value) || null,
-    lastPaymentDate: toISODateString(lastPaymentDateValue.value) || null,
+    firstPaymentDate: toISODateString(firstPaymentDateValue.value) || undefined,
+    lastPaymentDate: toISODateString(lastPaymentDateValue.value) || undefined,
   });
 
   // Reset form
